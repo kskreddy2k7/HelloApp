0a3e3b00c1f5f8b97533a2f3ee85e47c1632084e
Kata Sai Kranthu Reddy <kskreddy2k7@gmail.com>
2026-03-26 08:58:38 +0530

    Refactor name handling in HelloApp

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,14 +1,21 @@
 public class HelloApp {
     public static void main(String[] args) {
 
-        String names;
+        String result;
 
         if (args.length == 0) {
-            names = "World";
+            result = "World";
         } else {
-            names = String.join(", ", args);
+            StringBuilder sb = new StringBuilder();
+
+            for (String name : args) {
+                sb.append(name).append(", ");
+            }
+
+            // Remove last ", "
+            result = sb.substring(0, sb.length() - 2);
         }
 
-        System.out.println("Hello, " + names + "!");
+        System.out.println("Hello, " + result + "!");
     }
 }
